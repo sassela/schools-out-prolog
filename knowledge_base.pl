1cdc2b82a5d33e9aef4d7a13b4c36eee1295d30f
= <sassela@live.com>
2015-11-07 13:19:00 +0000

    Add facts to knowledge base

---
+++ knowledge_base.pl
@@ -0,0 +1,26 @@
+% Problem #2: School’s Out!
+
+teacher(knight).
+teacher(gross).
+teacher(evoy).
+teacher(appleton).
+teacher(parnell).
+
+activity(nudist).
+activity(bodyboarding).
+activity(swimming).
+activity(sightseeing).
+activity(camping).
+
+county(suffolk).
+county(cornwall).
+county(norfolk).
+county(yorkshire).
+county(cumbria).
+county(hertfordshire).
+
+subject(maths).
+subject(science).
+subject(english).
+subject(history).
+subject(pe).
