f5b6319de198dd40e695cf0f1bc3f64b3f823dce
= <sassela@live.com>
2015-11-07 13:38:35 +0000

    Begin adding rules to knowledge base

--- knowledge_base.pl
+++ knowledge_base.pl
@@ -24,3 +24,56 @@ subject(science).
 subject(english).
 subject(history).
 subject(pe).
+
+
+solve :-
+	activity(KnightActivity), 
+	activity(GrossActivity), 
+	activity(EvoyActivity), 
+	activity(AppletonActivity), 
+	activity(ParnellActivity), 
+	all_different([KnightActivity, GrossActivity, EvoyActivity,
+		AppletonActivity, ParnellActivity]),
+
+	holiday(KnightHoliday), 
+	holiday(GrossHoliday), 
+	holiday(EvoyHoliday), 
+	holiday(AppletonHoliday), 
+	holiday(ParnellHoliday), 
+	all_different([KnightHoliday, GrossHoliday, EvoyHoliday,
+		AppletonHoliday, ParnellHoliday]),
+
+	subject(KnightSubject), 
+	subject(GrossSubject), 
+	subject(EvoySubject), 
+	subject(AppletonSubject), 
+	subject(ParnellSubject), 
+	all_different([KnightSubject, GrossSubject, EvoySubject,
+		AppletonSubject, ParnellSubject]),
+
+	county(KnightCounty), 
+	county(GrossCounty), 
+	county(EvoyCounty), 
+	county(AppletonCounty), 
+	county(ParnellCounty), 
+	all_different([KnightCounty, GrossCounty, EvoyCounty,
+		AppletonCounty, ParnellCounty]),
+
+	Quads = [[knight, KnightCounty, KnightActivity, KnightClass],
+		 [gross, GrossCounty, GrossActivity, GrossClass],
+		 [evoy, EvoyCounty, EvoyActivity, EvoyClass],
+		 [appleton, AppletonCounty, AppletonActivity, AppletonClass],
+		 [parnell, ParnellCounty, ParnellActivity, ParnellClass]]. %TODO continue rule def with ,s
+
+
+
+
+
+
+
+
+
+
+
+
+
